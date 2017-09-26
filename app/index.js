a0a553e1b488df4cf6d8b38cd050cd52daaf5085
Shihlin Lu <shihlinlu@gmail.com>
2017-09-25 23:22:05 -0600

    added popular component, prop-types

--- app/index.js
+++ app/index.js
@@ -1,17 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var PropTypes = require('prop-types');
+var App = require('./components/App');
 require('./index.css');
-
-// component definition
-class App extends React.Component {
-    render() {
-        return (
-            <div>
-                Hello React Training!
-            </div>
-        )
-    }
-}
 
 ReactDOM.render(
     <App />,
